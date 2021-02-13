fce5b129c318edb43015216b78d4dcfd485303c9
Jamie Birch work laptop <jbizzle5@gmail.com>
2021-02-13 13:25:19 +0000

    Sanity check for Android

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { RouteProp } from '@react-navigation/core';
 import { FrameNavigationProp } from 'react-nativescript-navigation';
 import { MainStackParamList } from './NavigationParamList';
+import { isAndroid } from "@nativescript/core";
 
 import { demos } from './linked-components/install';
 interface MenuProps {
@@ -26,6 +27,20 @@ export function Menu({ navigation }: MenuProps) {
     return (
         <scrollView>
             <stackLayout>
+                <button
+                    key={"sanity"}
+                    onTap={() => {
+                        if(isAndroid){
+                            for(let key in com.facebook){
+                                console.log(`com.facebook.${key}`);
+                            }
+                            return;
+                        }
+                        console.log(`Sanity check not yet implemented for iOS (plugin only implemented for Android).`);
+                    }}
+                >
+                    Sanity check
+                </button>
                 {demos.map((demo) => (
                     <button
                         key={demo.name}
