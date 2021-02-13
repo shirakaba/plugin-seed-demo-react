19b12884762ee8b4861c31f6c7f5c5c5fddaa793
Jamie Birch work laptop <jbizzle5@gmail.com>
2021-02-13 13:34:23 +0000

    Specify references.d.ts

--- references.d.ts
+++ references.d.ts
@@ -1,6 +1,6 @@
 /// <reference path="./node_modules/@nativescript/types-ios/lib/ios.d.ts" />
 /// <reference path="./node_modules/@nativescript/types-android/lib/android-26.d.ts" />
 /// <reference path="./node_modules/@nativescript/core/global-types.d.ts" />
-/// <reference path="./node_modules/@nativescript-community/plugin-seed/typings/java!react-native-0.63.4.d.ts" />
+/// <reference path="./node_modules/@nativescript-community/plugin-seed/references.d.ts" />
 
 declare const DEVELOPMENT: boolean;
